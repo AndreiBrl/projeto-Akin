711be0ae8a41353d68852df74e00e264845b39bf
Andrei <barbutodev@gmail.com>
2023-12-29 18:59:41 -0300

    react reader funcionando

--- src/components/pages/Leitura.js
+++ src/components/pages/Leitura.js
@@ -2,19 +2,24 @@ import React from 'react';
 import { ReactReader } from 'react-reader';
 
 const LeituraEPUB = () => {
-  // Substitua com o caminho real para o seu arquivo EPUB
-  const epubUrl = 'https://magical-marzipan-1029e1.netlify.app/Original-Andrei_Barbuto.epub';
+    // Substitua com o caminho real para o seu arquivo EPUB
+    const epubUrl = 'https://magical-marzipan-1029e1.netlify.app/Original-Andrei_Barbuto.epub';
 
-  return (
-    <div style={{ position: 'relative', height: '100vh' }}>
-        <h1>teste</h1>
-      <ReactReader
-        url={epubUrl}
-        title="Exemplo de Leitura EPUB"
-        locationChanged={(epubcifi) => console.log(epubcifi)}
-      />
-    </div>
-  );
+    return (
+        <div className='containerLeitura'>
+            <h1>teste</h1>
+
+
+            <ReactReader
+                url={epubUrl}
+                title="Tribos Ancestrais"
+                locationChanged={(epubcifi) => console.log(epubcifi)}
+                swipeable='true'
+
+            />
+            <h1>teste</h1>
+        </div>
+    );
 };
 
 export default LeituraEPUB;
