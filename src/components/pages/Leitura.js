e7f7fcb7daf0b03bd5880c1f855c0b4a6325c576
Andrei <barbutodev@gmail.com>
2023-12-30 16:59:01 -0300

    atua reactReader

--- src/components/pages/Leitura.js
+++ src/components/pages/Leitura.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { ReactReader } from 'react-reader';
+import { ReactReader, ReactReaderStyle } from 'react-reader';
+import menuLeitura from '../img/menuLeitura.png'
 
 const LeituraEPUB = () => {
     // Substitua com o caminho real para o seu arquivo EPUB
     const epubUrl = 'https://magical-marzipan-1029e1.netlify.app/Original - Andrei Barbuto.epub';
     const [TotcurrentLocation, setTotCurrentLocation] = useState("");
-
+    const [changeZIndex, setChangeZindex] = useState(2)
     const [flag, setFlag] = useState(true);
 
     useEffect(() => {
@@ -31,27 +32,56 @@ const LeituraEPUB = () => {
 
     return (
         <div className="containerLeitura">
-            {/* <input onChange={(e) => setInputPage(e.target.value)}></input>
-            <button onClick={handlePage}>ir</button> */}
+            <div className='menuLeitura'>
 
-
+                <img src={menuLeitura} style={{ zIndex: changeZIndex }} />
+            </div>
             {/* flag criada por conta de que a propriedade location da bug na primeira rendezação de tela. Segundo a própria documentação a funcao 
 location é executada inevitavelmente assim que entra na tela.
 */}
+
             {flag ? (
                 <ReactReader
                     url={epubUrl}
-                    title="Tribos Ancestrais"
+
+
                     locationChanged={(location) => handleLocation(location)}
                     showToc={true}
                 />
             ) : (
                 <ReactReader
                     url={epubUrl}
-                    title="Tribos Ancestrais"
+
                     locationChanged={(location) => handleLocation(location)}
+
                     location={TotcurrentLocation}
+                    readerStyles={{
+                        ...ReactReaderStyle,
+
+                        tocArea: {
+                            ...ReactReaderStyle.tocArea,
+                            marginTop: '30vw'
+                        },
+                        tocButton: {
+                            ...ReactReaderStyle.tocButton,
+                            marginTop: '17vw',
+                            
+                        },
+                        readerArea: {
+                            ...ReactReaderStyle.readerArea,
+                            marginTop:'11vw',
+                            
+
+                            
+                        },
+                       
+                        
+
+
+                    }}
+
                     showToc={true}
+                    tocChanged={(e) => console.log(e)}
                 />
             )}
         </div>
