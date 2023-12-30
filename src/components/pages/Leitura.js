fe9ec3901c0d158c8057463d8d34be70c0addb01
Andrei <barbutodev@gmail.com>
2023-12-30 01:20:26 -0300

    react reader funcionando

--- src/components/pages/Leitura.js
+++ src/components/pages/Leitura.js
@@ -1,76 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ReactReader } from 'react-reader';
 
 const LeituraEPUB = () => {
     // Substitua com o caminho real para o seu arquivo EPUB
     const epubUrl = 'https://magical-marzipan-1029e1.netlify.app/Original - Andrei Barbuto.epub';
-    const [currentPage, setCurrentpage] = useState(0)
-    const [inputPage, setInputPage] = useState(0)
-    const [TotcurrentLocation, setTotCurrentLocation] = useState('epubcfi(/6/4!/4/2[pg-header]/8[pg-start-separator]')
-    const [countPage, setCountPage] = useState(0)
-    const [flag, setFlag] = useState(true)
-    const handlePage = () => {
-        console.log('clicou');
-        if (inputPage < 12) {
+    const [TotcurrentLocation, setTotCurrentLocation] = useState("");
 
-            setCurrentpage(inputPage)
-        }
-        else {
-            console.log('nao tem');
-        }
+    const [flag, setFlag] = useState(true);
 
-    }
+    useEffect(() => {
+        // Recupere a localização do localStorage ao montar o componente
+        const savedLocation = localStorage.getItem('epubLocation');
+        if (savedLocation) {
+            setTotCurrentLocation(savedLocation);
+            setFlag(false);
+        }
+    }, []);
+
+
 
     const handleLocation = (location) => {
-        if (location != 'epubcfi(/6/4!/4/2/1:0)') {
-
-            setFlag(false)
-            setTotCurrentLocation(location.toString())
+        if (location !== 'epubcfi(/6/4!/4/2/1:0)'&& TotcurrentLocation!="") {
+            console.log(location);
+            setFlag(false);
+            setTotCurrentLocation(location.toString());
+            // Salve a localização no localStorage
+            localStorage.setItem('epubLocation', location.toString());
         }
+    };
 
-
-    }
     return (
-        <div className='containerLeitura'>
-
-            <input onChange={(e) => setInputPage(e.target.value)}></input>
-            <button onClick={handlePage}>ir</button>
-
+        <div className="containerLeitura">
+            {/* <input onChange={(e) => setInputPage(e.target.value)}></input>
+            <button onClick={handlePage}>ir</button> */}
 
 
-
-            {
-                flag ? (
-                    < ReactReader
-                        url={epubUrl}
-                        title="Tribos Ancestrais"
-                        locationChanged={(location) => handleLocation(location)}
-
-                        // tocChanged={(location) => handleLocation(location)}
-
-                        // epubOptions={{ width: '100%', height: '50%' }}
-                        showToc={true}
-
-
-
-
-
-                    />) : (<ReactReader
-                        url={epubUrl}
-                        title="Tribos Ancestrai"
-                        locationChanged={(location) => handleLocation(location)}
-                        location={TotcurrentLocation}
-                        tocChanged={(location) => handleLocation(location)}
-                        // epubOptions={{ width: '100%', height: '50%' }}
-                        showToc={true}
-
-
-
-
-
-                    />)
-            }
-
+            {/* flag criada por conta de que a propriedade location da bug na primeira rendezação de tela. Segundo a própria documentação a funcao 
+location é executada inevitavelmente assim que entra na tela.
+*/}
+            {flag ? (
+                <ReactReader
+                    url={epubUrl}
+                    title="Tribos Ancestrais"
+                    locationChanged={(location) => handleLocation(location)}
+                    showToc={true}
+                />
+            ) : (
+                <ReactReader
+                    url={epubUrl}
+                    title="Tribos Ancestrais"
+                    locationChanged={(location) => handleLocation(location)}
+                    location={TotcurrentLocation}
+                    showToc={true}
+                />
+            )}
         </div>
     );
 };
