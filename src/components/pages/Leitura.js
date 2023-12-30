4e03692c84edba011e238b616c1869187b6f06fc
Andrei <barbutodev@gmail.com>
2023-12-30 00:37:28 -0300

    react reader funcionando

--- src/components/pages/Leitura.js
+++ src/components/pages/Leitura.js
@@ -1,23 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactReader } from 'react-reader';
 
 const LeituraEPUB = () => {
     // Substitua com o caminho real para o seu arquivo EPUB
-    const epubUrl = 'https://magical-marzipan-1029e1.netlify.app/Original-Andrei_Barbuto.epub';
+    const epubUrl = 'https://magical-marzipan-1029e1.netlify.app/Original - Andrei Barbuto.epub';
+    const [currentPage, setCurrentpage] = useState(0)
+    const [inputPage, setInputPage] = useState(0)
+    const [TotcurrentLocation, setTotCurrentLocation] = useState('epubcfi(/6/4!/4/2[pg-header]/8[pg-start-separator]')
+    const [countPage, setCountPage] = useState(0)
+    const [flag, setFlag] = useState(true)
+    const handlePage = () => {
+        console.log('clicou');
+        if (inputPage < 12) {
 
+            setCurrentpage(inputPage)
+        }
+        else {
+            console.log('nao tem');
+        }
+
+    }
+
+    const handleLocation = (location) => {
+        if (location != 'epubcfi(/6/4!/4/2/1:0)') {
+
+            setFlag(false)
+            setTotCurrentLocation(location.toString())
+        }
+
+
+    }
     return (
         <div className='containerLeitura'>
-            <h1>teste</h1>
+
+            <input onChange={(e) => setInputPage(e.target.value)}></input>
+            <button onClick={handlePage}>ir</button>
 
 
-            <ReactReader
-                url={epubUrl}
-                title="Tribos Ancestrais"
-                locationChanged={(epubcifi) => console.log(epubcifi)}
-                swipeable='true'
 
-            />
-            <h1>teste</h1>
+
+            {
+                flag ? (
+                    < ReactReader
+                        url={epubUrl}
+                        title="Tribos Ancestrais"
+                        locationChanged={(location) => handleLocation(location)}
+
+                        // tocChanged={(location) => handleLocation(location)}
+
+                        // epubOptions={{ width: '100%', height: '50%' }}
+                        showToc={true}
+
+
+
+
+
+                    />) : (<ReactReader
+                        url={epubUrl}
+                        title="Tribos Ancestrai"
+                        locationChanged={(location) => handleLocation(location)}
+                        location={TotcurrentLocation}
+                        tocChanged={(location) => handleLocation(location)}
+                        // epubOptions={{ width: '100%', height: '50%' }}
+                        showToc={true}
+
+
+
+
+
+                    />)
+            }
+
         </div>
     );
 };
