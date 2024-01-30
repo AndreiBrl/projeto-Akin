8282d35325cf57a3870abda8ab9afbfb3a219154
Andrei <barbutodev@gmail.com>
2024-01-30 17:20:11 -0300

    add map

--- src/components/pages/Mapa.js
+++ src/components/pages/Mapa.js
@@ -1,12 +1,18 @@
 import mapa from '../img/mapOri.jpg'
-
+import setaVolta from '../img/setaVolta.png'
+import { useNavigate } from "react-router-dom";
 
 const Mapa = () => {
+    const navigation = useNavigate();
+    const navega = (rota) => {
+        navigation(rota)
 
-    return(
+    }
+    return (
         <div className='pagMapa'>
 
-        <img  src={mapa}/>
+            <img className='oriMap' src={mapa} />
+            <img className='setaVolta' src={setaVolta} onClick={()=>{navega('/Leitura')}}/>
         </div>
     )
 }
