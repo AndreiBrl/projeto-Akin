a5e92e99d71ce00a963e5087092509d09c4c918a
Andrei <barbutodev@gmail.com>
2024-02-10 12:24:53 -0300

    atua

--- src/components/pages/Apoie.js
+++ src/components/pages/Apoie.js
@@ -32,9 +32,9 @@ const Apoie = () => {
                 <div className='textoApoie'>
                     <div className='textoApoie-2'>
 
-                        <p>Iniciamente, criei a história de "Tribos ancestrais" para ser no formato de livro, totalmente escrita e sem imagens. Um tempo depois, comecei a criar imagens com a IA e mudei completamente de ideia. A história começou a fazer muito mais sentido se fosse um mangá do que um livro. Então eu decidi que começaria a aprender a desenhar para transforma essa obra em um mangá. </p>
+                        <p>Iniciamente, criei a história de "Tribos ancestrais" para ser no formato de livro, totalmente escrita e sem imagens. Um tempo depois, comecei a criar imagens com IA e mudei completamente de ideia. A história começou a fazer muito mais sentido se fosse um mangá do que um livro. Então, decidi que começaria a aprender a desenhar para transformar essa obra em um mangá. </p>
                         <p>Se você está curtindo todo meu conteúdo e quer ver essa história virar um mangá de verdade, pode fazer uma doação para me ajudar a aprender a desenhar e continuar criando mais!</p>
-                        <p><b>OBS:</b> Vou continuar postando mais capítulos neste formato, usando IA para gerar as imagens, enquanto aprendo desenhar!<br/> <br/><b>Obrigado por tudo galera!!</b></p>
+                        <p><b>OBS:</b> Vou continuar postando mais capítulos neste formato, usando IA para gerar as imagens, enquanto aprendo a desenhar!<br/> <br/><b>Obrigado por tudo galera!!</b></p>
                         <p><b>PIX: </b>contatoandreibr@gmail.com</p>
                         <p> Titular: Andrei Barbuto Romanelli Lopes</p>
                     </div>
